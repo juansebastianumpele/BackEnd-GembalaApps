b7016f0591f5359f1005bcc14c09b991c5b9bd32
muhammadhanif010 <muhammad.hanif010@binus.ac.id>
2022-10-07 11:33:31 +0700

    Penambahan data varietas dan kandang pada seeders

--- seeders/20221006061847-varietas.js
+++ seeders/20221006061847-varietas.js
@@ -2,17 +2,72 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('d_varietas', [{
-      varietas: 'varietas 1',
-    },{
-      varietas: 'varietas 2',
-    },{
-      varietas: 'varietas 3',
-    }], {});
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('d_varietas', [
+      {
+        varietas: 'Garut'
+      },
+      {
+        varietas: 'Texel'
+      },
+      {
+        varietas: 'Dorper'
+      },
+      {
+        varietas: 'Merino'
+      },
+      {
+        varietas: 'Dombos'
+      },
+      {
+        varietas: 'Ekor Tipis'
+      },
+      {
+        varietas: 'Ekor Gemuk'
+      },
+      {
+        varietas: 'Batur'
+      },
+      {
+        varietas: 'St. Croix'
+      },
+      {
+        varietas: 'Suffolk'
+      },
+      {
+        varietas: 'Barbados Blackbelly'
+      },
+      {
+        varietas: 'Kisar'
+      },
+      {
+        varietas: 'Donggala'
+      },
+      {
+        varietas: 'Indramayu '
+      },
+      {
+        varietas: 'Jonggol'
+      },
+      {
+        varietas: 'Rote'
+      },
+      {
+        varietas: 'Dorset'
+      },
+      {
+        varietas: 'Sumbawa'
+      },
+      {
+        varietas: 'Waringin'
+      },
+      {
+        varietas: 'Lainnya'
+      },
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('d_varietas', null, {});
   }
 };
