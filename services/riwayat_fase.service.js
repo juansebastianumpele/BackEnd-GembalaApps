a25b1b91729fa970f1fa9f3e31b3a560e183e334
Putra Prassiesa Abimanyu <ppabimanyu@gmail.com>
2022-11-07 15:41:41 +0700

    feat: riwayat fase

---
+++ services/riwayat_fase.service.js
@@ -0,0 +1,15 @@
+
+// Riwayat Fase Service
+module.exports = async (db, req, dataTernak) => {
+    // Create riwayat fase
+    const riwayatFase = await db.RiwayatFase.create({
+        id_ternak: dataTernak.id_ternak,
+        id_fp: dataTernak.id_fp,
+        id_peternakan: req.dataAuth.id_peternakan,
+        tanggal: new Date()
+    });
+    if(!riwayatFase){
+        return false
+    }
+    return true
+}
