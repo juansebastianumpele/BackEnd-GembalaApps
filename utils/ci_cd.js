62a5c7df236a97dae92ef0cf12dd7d4b113596d2
Putra Prassiesa Abimanyu <ppabimanyu@gmail.com>
2022-11-16 15:44:50 +0700

    fix: ci//cd

--- utils/ci_cd.js
+++ utils/ci_cd.js
@@ -1,3 +1,5 @@
+const {execFile} = require('child_process');
+
 module.exports = (app) =>{
     app.post('/build-dashboard',(req,res) => {
         execFile('/home/sembadafarm/web/gembala.sembadafarm.com/pull.sh', (err, stdout, stderr) => {
