3d9f9a2bb2f1d4a687cf0f1790bc02c1ee565dec
Putra Prassiesa Abimanyu <ppabimanyu@gmail.com>
2022-11-16 16:35:55 +0700

    test

--- utils/ci_cd.js
+++ utils/ci_cd.js
@@ -3,13 +3,15 @@ const {execFile} = require('child_process');
 module.exports = (app) =>{
     app.post('/build-dashboard',(req,res) => {
         execFile('/home/sembadafarm/web/gembala.sembadafarm.com/pull.sh', (err, stdout, stderr) => {
-            if (err) {
-                console.error(err)
-                res.writeHead(404)
-                return res.end()
-            }    
-            res.writeHead(200)
-            res.end()
+            // if (err) {
+            //     console.error(err)
+            //     res.writeHead(404)
+            //     return res.end()
+            // }    
+            // res.writeHead(200)
+            // res.end()
         })
+        res.send('ok')
+        res.end()
     })
 }
