062ffdb46470112e65c721761b7b34a3bc7f44e2
Putra Prassiesa Abimanyu <ppabimanyu@gmail.com>
2022-11-23 09:49:17 +0700

    fix: config

--- config/app.config.js
+++ config/app.config.js
@@ -6,11 +6,11 @@ module.exports = {
         secret: "$2a$19$QeyGVx5HvztvllwoKJnCBea1AMJCTUAioDLPepc7fCS.Sjd.ZdD3O",
         expiresIn: 18000
     },
-    url: URL,
-    auth:{
-      email: "test.gembala@gmail.com",
-      password: "gembalapp1",
-      password_app: "owoawahhflzhwkcf",
-      url_redirect_verify: "https://gembala.netlify.app/verify/success-verify"
-    }
+  url: URL,
+  auth:{
+    email: "test.gembala@gmail.com",
+    password: "gembalapp1",
+    password_app: "owoawahhflzhwkcf",
+    url_redirect_verify: "gembala.sembadafarm.com/verify/success-verify"
+  }
 }
